e5a377fdb6b4f625d5e7da19f05e80fd699d0fa6
JunsangHan <jshan0703@gmail.com>
2023-05-07 17:56:13 +0900

    home 페이지 임시 그룹화 적용

--- home.py
+++ home.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import pandas as pd
 
 
 def render():
-    st.title('Home')
+    st.title('2023 개인사업자 캠프 상반기 워크샵!')
     st.write('Welcome to the home page!')
+
+    # 조별 사람들의 데이터 생성
+    data = {
+        'Group A': ['Person A1', 'Person A2', 'Person A3', 'Person A4', 'Person A5'],
+        'Group B': ['Person B1', 'Person B2', 'Person B3', 'Person B4', 'Person B5'],
+        'Group C': ['Person C1', 'Person C2', 'Person C3', 'Person C4', 'Person C5'],
+        'Group D': ['Person D1', 'Person D2', 'Person D3', 'Person D4', 'Person D5']
+    }
+
+    # DataFrame 생성
+    df = pd.DataFrame(data)
+
+    # 조별 사람들의 데이터 표시
+    st.write(df)
+    '''
+    columns = st.beta_columns(num_groups)
+    for i, group in enumerate(st.session_state.displayed_members, 1):
+    with columns[i-1]:
+        st.subheader(f"조 {i}")
+        for member in group:
+            st.text(member)
+    '''
+
