ebf7a853fbdc4d73381837e31d58ba9f8016512b
JunsangHan <jshan0703@gmail.com>
2023-05-08 23:30:03 +0900

    home 페이지에 직군 그래프까지 1차 구현 완료

--- home.py
+++ home.py
@@ -12,16 +12,48 @@ def render_graph():
     G = nx.Graph()
 
     for index, row in data.iterrows():
-        G.add_node(row['이름'], cell=row['셀'], group=row['직군'], domain1=row['도메인1'], domain2=row['도메인2'])
+        G.add_node(row['이름'], group=row['직군'], cell=row['셀'], domain1=row['도메인1'], domain2=row['도메인2'])
 
-        if not pd.isna(row['도메인1']):
-            G.add_edge(row['이름'], row['도메인1'])
-
-        if not pd.isna(row['도메인2']):
-            G.add_edge(row['이름'], row['도메인2'])
+        # 같은 속성을 공유하는 노드 사이의 엣지 추가
+    for i, row_i in data.iterrows():
+        for j, row_j in data.iterrows():
+            if i != j:
+                if row_i['직군'] == row_j['직군'] or row_i['셀'] == row_j['셀'] or row_i['도메인1'] == row_j['도메인1'] \
+                        or row_i['도메인2'] == row_j['도메인2']:
+                    G.add_edge(row_i['이름'], row_j['이름'])
 
     # Pyvis 네트워크로 변환 및 시각화
-    nt = Network(notebook=True)
+    nt = Network(notebook=True, height='800px', width='100%', bgcolor='#222222', font_color='white')
+
+    # Configure physics options
+    nt.options = {
+        "physics": {
+            "stabilization": {
+                "enabled": True,
+                "iterations": 1000,
+                "updateInterval": 50,
+                "onlyDynamicEdges": False
+            },
+            "barnesHut": {
+                "gravitationalConstant": -4000,
+                "centralGravity": 0.2,
+                "springLength": 200,
+                "springConstant": 0.01,
+                "damping": 0.9,
+                "avoidOverlap": 1
+            },
+            "minVelocity": 0.5,
+            "maxVelocity": 10,
+        },
+        "nodes": {
+            "font": {"size": 15, "face": "tahoma"}
+        },
+        "edges": {
+            "color": {"color": "#FFFFFF"},
+            "font": {"size": 12, "face": "tahoma"},
+            "arrows": {"to": {"enabled": False}}
+        }
+    }
     nt.from_nx(G)
     nt.show('people_graph.html')
     st.write("People Graph:")
@@ -50,4 +82,3 @@ def render():
                 st.write(member)
 
     render_graph()
-
