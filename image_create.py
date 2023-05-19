aab0310e39a3e8a841467e1be6219a0707fdf27a
JunsangHan <jshan0703@gmail.com>
2023-05-19 15:08:37 +0900

    [feature] 보너스 페이지 완료

--- image_create.py
+++ image_create.py
@@ -3,6 +3,13 @@ import openai
 
 
 def render():
+    st.title('보너스 페이지 👩‍🎨👨‍🎨🖼')
+    st.text('\n')
+    st.text('조별 미션 하느라 고생 많으셨습니다.')
+    st.text('아래 입력창에 텍스트를 입력하면 AI가 그림을 그려서 보여줍니다.')
+    st.text('조별 미션하면서 찍은 사진을 AI가 그려줄 수 있도록 텍스트를 입력해보세요.')
+    st.text('포즈만 비슷해도 푸짐한 보너스 점수가!!')
+    st.text('Hint: 영어로 입력해야 잘 그려줍니다. 번역기를 활용해보세요😃😄😆')
     prompt = st.text_input("Enter the prompt for the image edit")
 
     if prompt:
